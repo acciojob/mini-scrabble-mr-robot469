d31bef6d7fefc2bbbc9d994313e164b48be66b9a
Dh A Nu <82042269+mr-robot469@users.noreply.github.com>
2023-01-14 12:15:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your code here
+function myFunc(){
+    console.log("hello")
+        let a = document.getElementById("evaluatedText").value.length
+        document.getElementById("letterCount").innerHTML = a;
+}
+

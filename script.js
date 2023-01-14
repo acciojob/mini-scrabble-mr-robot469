22835ea15fb2128efd807f5bdc12e48f6a4c7aad
Dh A Nu <82042269+mr-robot469@users.noreply.github.com>
2023-01-14 13:11:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-function myFunc(){
-        let a = document.getElementById("evaluatedText").value.length
-        document.getElementById("letterCount").innerHTML = a;
-}
+let count;
+document.getElementById("evaluatedText").value = ""; // initially string is empty so count is also empty
 
+document.addEventListener("keyup", function () {
+  count = document.getElementById("evaluatedText").value; //here this value will return string
+  document.querySelector("#letterCount").textContent = count.length;
+  //   console.log(count);
+});

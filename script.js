6c783383c8aadbece17de724083a40e14102839b
Dh A Nu <82042269+mr-robot469@users.noreply.github.com>
2023-01-17 21:30:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let count;
-document.getElementById("evaluatedText").value = ""; // initially string is empty so count is also empty
 
-document.addEventListener("keyup", function () {
-  count = document.getElementById("evaluatedText").value; //here this value will return string
-  document.querySelector("#letterCount").textContent = count.length;
-  //   console.log(count);
-});
+let evaluatedText = document.getElementById("evaluatedText") // initially string is empty so count is also empty
+let letterCount = document.getElementById("letterCount")
+
+evaluatedText.addEventListener("input",()=>{
+		let count = evaluatedText.value.length;
+		 letterCount.innerHTML = count;
+})

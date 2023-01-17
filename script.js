3c56078864d79aab9d8649a7ba2cac1671a84595
Dh A Nu <82042269+mr-robot469@users.noreply.github.com>
2023-01-17 21:38:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
 
-let evaluatedText = document.getElementById("evaluatedText") // initially string is empty so count is also empty
-let letterCount = document.getElementById("letterCount")
-
-evaluatedText.addEventListener("input",()=>{
-		let count = evaluatedText.value.length;
-		 letterCount.innerHTML = count;
-})
